a21e5ccd9bf7aaac82d718af5ba4342089ba5d17
Segura-David <perzeus123@gmail.com>
2023-05-09 17:24:45 -0600

    Empiezo con mi curso de javascript:
    Todo lo de variables ya lo tengo mas que entendido, entonces me saltee
    a la parte de operadores solamente para repasar desde alli.

---
+++ repasos/JS/operadores.js
@@ -0,0 +1,23 @@
+let x = 10;
+let y = 6;
+
+// operadores de asignacion
+
+// x = 6;
+// x += 6;
+// x -= 6;
+// x *= 6;
+// x /= 6;
+// x %= 6;
+// x **= 6;
+
+// operadores aritmeticos
+
+x + y;
+x - y;
+x * y;
+x / y;
+x % y;
+x ** y;
+x++;
+x--;
