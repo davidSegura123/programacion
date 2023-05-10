46494aa40397ffbb006841995371671d9208fa50
Segura-David <perzeus123@gmail.com>
2023-05-10 03:56:23 -0600

    Aprendiendo Bootstrap:
    El curso que sigo me va a llevar por un muy buen camino.

--- repasos/JS/otros.js
+++ repasos/JS/otros.js
@@ -1,5 +1,5 @@
 // concatenacion y backticks
 
-let nombre = "David Segura";
-let frase = "Hola, soy " + nombre + " y estudio programacion";
-let frase2 = `Hola, soy ${nombre} y estudio programacion.`;
+// let nombre = "David Segura";
+// let frase = "Hola, soy " + nombre + " y estudio programacion";
+// let frase2 = `Hola, soy ${nombre} y estudio programacion.`;
