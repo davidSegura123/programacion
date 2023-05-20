4031a21ecf273020b925f354ffd86ae345e81138
David-Segura <davidPerzeus123@gmail.com>
2023-05-20 14:43:39 -0300

    cambios

---
+++ carpeta/php/tp195/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            Inicio
+        </title>
+    </head>
+    <body>
+        <form action="main.php" method="post">
+            <select name="lista1" required>
+                <option value="1">Alambre</option>
+                <option value="2">Cemento</option>
+                <option value="3">Cal</option>
+                <option value="4">Pan con Coca</option>
+            </select>
+        </form>
+        <textarea name="" cols="30" rows="10" required>
+
+        </textarea>
+    </body>
+</html>
